d58d373e78f6274f5ad126b7ee1cdf25f57ad962
jurchiks <jurchiks101@gmail.com>
2016-03-27 12:01:38 +0300

    Adding a static version of the DataAccessor trait.

---
+++ src/traits/StaticDataAccessor.php
@@ -0,0 +1,128 @@
+<?php
+namespace js\tools\commons\traits;
+
+trait StaticDataAccessor
+{
+	private static $data = [];
+	
+	protected static function init(array $data)
+	{
+		self::$data = $data;
+	}
+	
+	public static function exists(string $name)
+	{
+		if (isset(self::$data[$name]))
+		{
+			return true;
+		}
+		
+		if (strpos($name, '.') === false)
+		{
+			return false;
+		}
+		
+		$parts = explode('.', $name);
+		$found = [];
+		$value = null;
+		
+		foreach ($parts as $key)
+		{
+			if (is_null($value))
+			{
+				// first level
+				if (isset(self::$data[$key]))
+				{
+					$value = self::$data[$key];
+					$found[$key] = true;
+				}
+			}
+			else if (isset($value[$key]))
+			{
+				// nested levels, e.g. $name = "database.host"
+				$value = $value[$key];
+				$found[$key] = true;
+			}
+		}
+		
+		unset($value);
+		
+		return ($parts === array_keys($found));
+	}
+	
+	/**
+	 * @param string $name : the name of the config property to retrieve. Can be dot-separated for access to nested
+	 *     properties, e.g. "database.host" will retrieve $config["database"]["host"] if it exists
+	 * @param mixed $default : the default value to return if property was not found
+	 * @return mixed whatever the config value or default value is
+	 */
+	public static function get(string $name, $default = null)
+	{
+		if (isset(self::$data[$name]))
+		{
+			return self::$data[$name];
+		}
+		
+		if (strpos($name, '.') === false)
+		{
+			return $default;
+		}
+		
+		$parts = explode('.', $name);
+		$value = null;
+		
+		foreach ($parts as $key)
+		{
+			if (is_null($value))
+			{
+				// first level
+				if (isset(self::$data[$key]))
+				{
+					$value = self::$data[$key];
+				}
+			}
+			else if (isset($value[$key]))
+			{
+				// nested levels, e.g. $name = "database.host"
+				$value = $value[$key];
+			}
+		}
+		
+		return (is_null($value) ? $default : $value);
+	}
+	
+	public static function getInt(string $name, int $default = 0): int
+	{
+		$value = self::get($name);
+		
+		return (is_numeric($value) ? intval($value) : $default);
+	}
+	
+	public static function getFloat(string $name, float $default = 0): float
+	{
+		$value = self::get($name);
+		
+		return (is_numeric($value) ? floatval($value) : $default);
+	}
+	
+	public static function getString(string $name, string $default = ''): string
+	{
+		$value = self::get($name);
+		
+		return (!is_null($value) ? strval($value) : $default);
+	}
+	
+	public static function getBool(string $name, bool $default = false): bool
+	{
+		$value = self::get($name);
+		
+		return (!empty($value) ? $value : $default);
+	}
+	
+	public static function getArray(string $name, array $default = []): array
+	{
+		$value = self::get($name);
+		
+		return (is_array($value) ? $value : $default);
+	}
+}

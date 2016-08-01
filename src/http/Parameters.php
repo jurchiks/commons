e5ef8c51e96ead476f71659de503c8eefc3a506b
js <js@rention.com>
2016-08-01 17:54:19 +0300

    URI Parameters - implement JsonSerializable.

--- src/http/Parameters.php
+++ src/http/Parameters.php
@@ -2,13 +2,19 @@
 namespace js\tools\commons\http;
 
 use js\tools\commons\traits\DataWriter;
+use JsonSerializable;
 
-class Parameters
+class Parameters implements JsonSerializable
 {
 	use DataWriter;
 	
 	public function __construct(array $data)
 	{
 		$this->init($data);
+	}
+	
+	function jsonSerialize()
+	{
+		return $this->getAll();
 	}
 }
